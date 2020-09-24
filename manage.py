02fb694a2f976183ad3d56d8f652293fa812636a
Mornica Mwende <mornicamwende@gmail.com>
2020-09-24 16:55:51 +0300

    Deployment

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Comment, Pitch, PitchCategory
 
 
 
-# app = create_app('production')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
