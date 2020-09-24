072a222b7577107dc81032647d15438ccaa29d23
Mornica Mwende <mornicamwende@gmail.com>
2020-09-24 13:27:35 +0300

    added user forms for accesing user data

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,7 +14,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
         if User.query.filter_by(username=data_field.data).first():
-            raise ValidationError('There is an account with that email')
+            raise ValidationError('There is an account created with that email')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username=data_field.data).first():
