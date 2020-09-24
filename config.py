b0b12994255bc5047e5a225d16e107f8e8a7ec39
Mornica Mwende <mornicamwende@gmail.com>
2020-09-24 17:55:42 +0300

    Deployment

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY = 'monicah'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
